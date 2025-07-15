c109cc8fcca3b9b4b219a369c99a9e36aeb14a8b
stephanosiame <siamelstephano@gmail.com>
2025-07-15 22:47:47 +0300

    update all

--- api/agrilink/settings.py
+++ api/agrilink/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'agrilink.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'agrilink_db',
+        'USER': 'postgres',
+        'PASSWORD': 'siame2003',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
